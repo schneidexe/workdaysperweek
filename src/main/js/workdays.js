dbd7a0d060ca6122bb3293e000c4a0bfe5d4386d
schneidexe <schneidexe@schneidexe.de>
2013-04-22 01:47:17 +0200

    - german holidays - fixed and easter dependent - general and regional

---
+++ src/main/js/workdays.js
@@ -0,0 +1,128 @@
+var moment = require("moment");
+
+var states = {
+        de : [ 'bw', 'by', 'be', 'bb', 'hb', 'hh', 'he', 'mv', 'ni', 'nw', 'rp', 'sl', 'sn', 'st', 'sh', 'th' ]
+    },
+    easterOffsets = {
+        de : {
+            '-2' : [ ],                                  // good friday
+            1 : [ ],                                     // easter monday
+            39 : [ ],                                    // ascension day
+            50 : [ ],                                    // whit monday
+            60 : [ 'bw', 'by', 'he', 'nw', 'rp', 'sl' ]  // corpus christi
+        }
+    },
+    holidays = {
+        de : {
+            '01/01' : [ ],                               // new year's day
+            '01/06' : [ 'bw', 'by', 'st' ],              // holy 3 kings
+            '05/01' : [ ],                               // labor day
+            '08/15' : [ 'by', 'sl' ],                    // assumption day
+            '10/03' : [ ],                               // reunion day
+            '10/31' : [ 'bb', 'mv', 'sn', 'st', 'th' ],  // reformation day
+            '11/01' : [ 'bw', 'by', 'nw', 'rp', 'sl' ],  // holy saints
+            '11/20' : [ 'sn' ],                          // penance day
+            '12/25' : [ ],                               // christmas day
+            '12/26' : [ ]                                // boxing day
+        }
+    };
+
+workDays = function(country, state, week, year) {
+    // validation
+    if (country === undefined || country === null )
+        throw "country not defined";
+    if (states[country] === undefined)
+        throw "country not found";
+    if (state === undefined || state === null)
+        throw "state not defined";
+    if (states[country].indexOf(state) < 0)
+        throw "state not found";
+    if (week === undefined)
+        week = moment().isoWeek();
+    if (week === null || isNaN(parseInt(week)) || week < 1 || week > 53)
+        throw "week has to be a positive number [1..53]";
+    if (year === undefined)
+        year = moment().year();
+    if (year === null || isNaN(parseInt(year)) || year < 0)
+        throw("year has to be a positive number");
+
+    // get start of week
+    var weekStartDate = moment().year(year).week(week).startOf('week');
+
+    // check monday 'til friday
+    var workingDays = [];
+    for (day = 1; day < 6; day++) {
+        var date = moment(weekStartDate.day(day));
+        if (!isHoliday(country, state, date))
+            workingDays.push(date);
+    }
+
+    return workingDays;
+}
+
+isHoliday = function(country, state, date) {
+    console.log("checking date " + date.format());
+
+    // check for fix holiday
+    var countryFixHolidays = holidays[country];
+    for (var holiday in countryFixHolidays) {
+        var holidayDate = moment(date.year() + '/' + holiday);
+        if (holidayDate.isSame(date)) {
+            // general holidays
+            if (countryFixHolidays[holiday].length === 0) {
+                console.log(date.format() + " is a fix holiday");
+                return true;
+            // regional holidays
+            } else if (countryFixHolidays[holiday].indexOf(state) > -1) {
+                console.log(date.format() + " is a fix holiday");
+                return true;
+            }
+        }
+    }
+
+    // check for easter formula depending holidays
+    var countryEasterOffsets = easterOffsets[country];
+    for (var offset in countryEasterOffsets) {
+        var holidayDate = easterSunday(date.year()).add('days', offset);
+        if (holidayDate.isSame(date)) {
+            // general holidays
+            if (countryEasterOffsets[offset].length === 0) {
+                console.log(date.format() + " is a variable holiday");
+                return true;
+            // regional holidays
+            } else if (countryEasterOffsets[offset].indexOf(state) > -1) {
+                console.log(date.format() + " is a fix holiday");
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+easterSunday = function(year) {
+    if (year === undefined || year === null)
+        year = moment().year();
+
+    var a = year % 19,
+        b = year % 4,
+        c = year % 7,
+        k = parseInt(year / 100),
+        p = parseInt((8 * k + 13) / 25),
+        q = parseInt(k / 4),
+        m = (15 + k - p - q) % 30,
+        n = (4 + k - q) % 7,
+        d = (19 * a + m) % 30,
+        e = (2 * b + 4 * c + 6 * d + n) % 7,
+        o = 22 + d + e;
+
+    if (o >= 31 + 25)
+        o -= 7;
+
+    return moment([year, 2, 1]).date(o);
+}
+
+// exports
+exports.workDays = workDays;
+exports.isHoliday = isHoliday;
+exports.easterSunday = easterSunday;
---
+++ src/test/js/workdays.spec.js
@@ -0,0 +1,139 @@
+var workDays = require('../../main/js/workdays'),
+    moment = require('moment');
+
+describe('test suite for work days per week', function () {
+    describe('test arguments', function () {
+        describe('country', function () {
+            it('throw exception if no country given', function () {
+                expect(function () {
+                    workDays.workDays()
+                }).toThrow("country not defined");
+            })
+            it('throw exception if country is null', function () {
+                expect(function () {
+                    workDays.workDays(null)
+                }).toThrow("country not defined");
+            })
+            it('throw exception if country is not existing', function () {
+                expect(function () {
+                    workDays.workDays('xx')
+                }).toThrow("country not found");
+            })
+        })
+        describe('state', function () {
+            it('throw exception if no state given', function () {
+                expect(function () {
+                    workDays.workDays('de')
+                }).toThrow("state not defined");
+            })
+            it('throw exception if state is null', function () {
+                expect(function () {
+                    workDays.workDays('de', null)
+                }).toThrow("state not defined");
+            })
+            it('throw exception if state is not existing', function () {
+                expect(function () {
+                    workDays.workDays('de', 'xx')
+                }).toThrow("state not found");
+            })
+        })
+        describe('week', function () {
+            it('throw exception if week is null', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', null)
+                }).toThrow("week has to be a positive number [1..53]");
+            })
+            it('throw exception if week is NaN', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', 'abc')
+                }).toThrow("week has to be a positive number [1..53]");
+            })
+            it('throw exception if week is zero', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', 0)
+                }).toThrow("week has to be a positive number [1..53]");
+            })
+            it('throw exception if week is negative', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', -1)
+                }).toThrow("week has to be a positive number [1..53]");
+            })
+            it('throw exception if week is to big', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', 54)
+                }).toThrow("week has to be a positive number [1..53]");
+            })
+            it('allow valid week values', function () {
+                expect(workDays.workDays('de', 'by', 1).length).toBeGreaterThan(0);
+                expect(workDays.workDays('de', 'by', 53).length).toBeGreaterThan(0);
+                expect(workDays.workDays('de', 'by', '20').length).toBeGreaterThan(0);
+            })
+            it('use current week if no week given', function () {
+                expect(workDays.workDays('de', 'by').length).toBeGreaterThan(0);
+            })
+        })
+        describe('year', function () {
+            it('throw exception if year is null', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', '1', null)
+                }).toThrow("year has to be a positive number");
+            })
+            it('throw exception if week is NaN', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', '1', 'abc')
+                }).toThrow("year has to be a positive number");
+            })
+            it('throw exception if year is negative', function () {
+                expect(function () {
+                    workDays.workDays('de', 'by', 1, -1000)
+                }).toThrow("year has to be a positive number");
+            })
+            it('use current year if no year given', function () {
+                expect(workDays.workDays('de', 'by', 1).length).toBeGreaterThan(0);
+            })
+        })
+    })
+    describe('easter formula', function () {
+        it('easter sunday in 1954 is April 18th', function () {
+            expect(workDays.easterSunday(1954).format()).toEqual(moment('1954/04/18').format());
+        })
+        it('easter sunday in 1981 is April 19th', function () {
+            expect(workDays.easterSunday(1981).format()).toEqual(moment('1981/04/19').format());
+        })
+        it('easter sunday in 2013 is March 31st', function () {
+            expect(workDays.easterSunday(2013).format()).toEqual(moment('2013/03/31').format());
+        })
+    })
+    describe('general holiday functionality', function () {
+        it('week 52/2012 has 3 working days (christmas)', function () {
+            expect(workDays.workDays('de', 'by', 52, 2012).length).toEqual(3);
+        })
+        it('week 1/2013 has 4 working days (new year\'s day)', function () {
+            expect(workDays.workDays('de', 'by', 1, 2013).length).toEqual(4);
+        })
+        it('week 13/2013 has 4 working days (good friday)', function () {
+            expect(workDays.workDays('de', 'by', 13, 2013).length).toEqual(4);
+        })
+        it('week 14/2013 has 4 working days (easter monday)', function () {
+            expect(workDays.workDays('de', 'by', 14, 2013).length).toEqual(4);
+        })
+        it('week 19/2013 has 4 working days (ascension)', function () {
+            expect(workDays.workDays('de', 'by', 19, 2013).length).toEqual(4);
+        })
+        it('working days in week 19/2013 are May 6-8th and 10th', function () {
+            var result = workDays.workDays('de', 'by', 19, 2013);
+            expect(result[0].format()).toEqual(moment('2013/05/06').format());
+            expect(result[1].format()).toEqual(moment('2013/05/07').format());
+            expect(result[2].format()).toEqual(moment('2013/05/08').format());
+            expect(result[3].format()).toEqual(moment('2013/05/10').format());
+        })
+    })
+    describe('regional holiday functionality', function () {
+        it('week 1/2012 has 4 working days in bavaria (holy 3 kings)', function () {
+            expect(workDays.workDays('de', 'by', 1, 2012).length).toEqual(4);
+        })
+        it('week 1/2012 has 5 working days in hamburg (no holy 3 kings)', function () {
+            expect(workDays.workDays('de', 'hh', 1, 2012).length).toEqual(5);
+        })
+    })
+})
